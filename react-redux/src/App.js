b409e1b0711ffb3a7d9fb4e7bd460960147464ba
Matheus Santos <matheus.santos@ilegra.com>
2019-11-21 16:40:51 -0300

    Added actions and reducers.

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLooged = useSelector(state => state.isLooged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(15))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {/* {isLooged ? <h3>Valuable Information I shouldn't see</h3> : ''} */}
+      {isLooged && <h3>Valuable Information I shouldn't see</h3>}
     </div>
   );
 }
--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -4,43 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
 
-//STORE -> GLOBALIZED STATE
+const myStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  };
-};
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
